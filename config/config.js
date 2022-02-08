5a9a27fd3949790950cf762a754a5e0cd271946d
ga16sweethome <98973673+ga16sweethome@users.noreply.github.com>
2022-02-08 22:37:46 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -19,6 +19,9 @@ module.exports = {
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DATABASE,
         host: process.env.POSTGRES_HOST,
-        dialect: "postgres"
+        dialect: "postgres",
+        ssl: {
+            rejectUnauthorized: false,
+        },
         },
     };
