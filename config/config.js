aab4a00a8aab5aa91f0cc1c040533a90969dc5b0
ga16sweethome <98973673+ga16sweethome@users.noreply.github.com>
2022-02-08 22:36:05 +0700

    Update config

--- config/config.js
+++ config/config.js
@@ -3,7 +3,6 @@ module.exports = {
     development: {
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
-        
         database: process.env.POSTGRES_DATABASE,
         host: process.env.POSTGRES_HOST,
         dialect: "postgres",
@@ -16,13 +15,10 @@ module.exports = {
         dialect: "postgres",
     },
     production: {
-        use_env_variable: process.env.DATABASE_URL,
-        dialect: "postgres",
-        protocol: "postgres",
-        dialectOptions: {
-        ssl: {
-            rejectUnauthorized: false,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        host: process.env.POSTGRES_HOST,
+        dialect: "postgres"
         },
-        },
-    },
     };
