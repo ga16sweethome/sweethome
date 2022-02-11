aaca1796a2815a3a5b43f52799415a75d9c92286
affandiagung <ccoc.20001@gmail.com>
2022-02-11 14:07:58 +0700

    logicnya masih kurang di showcase filter

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,21 @@
-const express = require("express")
-const router = express.Router()
-const homepage = require('./homepage')
-const showcase = require("./showcase")
-const user = require('./user')
-const content = require("./content")
-const timeslot = require("./timeslot")
-const project = require('./project')
-const favorite = require("./favorite")
-const appointment = require("./appointment")
+const express = require("express");
+const router = express.Router();
+const homepage = require("./homepage");
+const showcase = require("./showcase");
+const user = require("./user");
+const content = require("./content");
+const timeslot = require("./timeslot");
+const project = require("./project");
+const favorite = require("./favorite");
+const appointment = require("./appointment");
 
-router.use("/homepage",homepage)
-router.use("/user", user)
-router.use("/showcase",showcase)
-router.use("/content", content)
-router.use("/timeslot", timeslot)
-router.use("/appointment", appointment)
-router.use("/project",project)
-router.use("/favorite",favorite)
+router.use("/homepage", homepage);
+router.use("/user", user);
+router.use("/showcase", showcase);
+router.use("/content", content);
+router.use("/timeslot", timeslot);
+router.use("/appointment", appointment);
+router.use("/project", project);
+router.use("/favorite", favorite);
 
-module.exports = router
+module.exports = router;
