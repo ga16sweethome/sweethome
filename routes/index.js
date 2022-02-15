122e5a48cec4338d9404d7666ebaaa84de3e790a
affandiagung <ccoc.20001@gmail.com>
2022-02-15 14:31:02 +0700

    + routes overview n customer

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ const timeslot = require("./timeslot");
 const project = require("./project");
 const favorite = require("./favorite");
 const appointment = require("./appointment");
+const overview = require("./overview");
+const customer = require("./customer");
 const { getPicture } = require("../controllers/user");
 
 router.use("/user", user);
@@ -17,5 +19,7 @@ router.use("/appointment", appointment);
 router.use("/project", project);
 router.use("/favorite", favorite);
 router.use("/", getPicture);
+router.use("/overview", overview);
+router.use("/customer", customer);
 
 module.exports = router;
