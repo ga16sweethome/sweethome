83aecfe9696d496c5dee1fe3465ab9bd02384da4
affandiagung <ccoc.20001@gmail.com>
2022-02-17 09:39:55 +0700

    overview

--- controllers/overview.js
+++ controllers/overview.js
@@ -19,23 +19,15 @@ module.exports = {
       let end = moment().tz("UTC").endOf("days").add(1, "days").toDate();
       let TODAY = moment(new Date()).format("YYYY-MM-DD");
       let besok = moment(new Date()).add(1, "days").format("YYYY-MM-DD");
-      let lusa = moment(new Date()).add(2, "days").format("YYYY-MM-DD");
+      let lusa = moment(new Date())
+        .add(2, "days")
+        .add(7, "hours")
+        .format("YYYY-MM-DD");
 
       let cari = await Appointment.findAll({
         where: {
           appointmentDate: {
-            [Op.or]: [
-              {
-                from: {
-                  [Op.between]: [start, end],
-                },
-              },
-              {
-                to: {
-                  [Op.between]: [start, end],
-                },
-              },
-            ],
+            [Op.between]: [start, end],
           },
         },
         order: [["appointmentDate", "ASC"]],
