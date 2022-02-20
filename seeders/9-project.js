2e0d14ceb1915246e604c39f39f3356a9eecd570
affandiagung <ccoc.20001@gmail.com>
2022-02-20 12:16:12 +0700

    migrate

--- seeders/9-project.js
+++ seeders/9-project.js
@@ -1,52 +1,57 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert(
-      'Projects', 
-      [{
-        code : "#P-0001",
-        appointmentId : 9,
-        uploadReceipt : "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/ReceiptSwiss.jpg/180px-ReceiptSwiss.jpg",
-        noteUploadReceipt : "bukti transfer",
-        userId : 4,
-        status : 3,
-        createdAt : new Date(),
-        updatedAt : new Date()
-       },
-       {
-        code : "#P-0002",
-        appointmentId : 7,
-        userId : 3,
-        status : 1,
-        createdAt : new Date(),
-        updatedAt : new Date()
-       },
-       {
-        code : "#P-0003",
-        appointmentId : 9,
-        userId : 5,
-        status : -1,
-        createdAt : new Date(),
-        updatedAt : new Date()
-       },
-      
-       
-      
-      
-      
-           
-     ], 
-     {});
-   
- }, 
+      "Projects",
+      [
+        {
+          code: "#P-0001",
+          appointmentId: 9,
+          uploadReceipt:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/ReceiptSwiss.jpg/180px-ReceiptSwiss.jpg",
+          noteUploadReceipt: "bukti transfer",
+          userId: 4,
+          status: 3,
+          totalPrice: 90000,
+          totalArea: 20,
+          totalDuration: 30,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          code: "#P-0002",
+          appointmentId: 7,
+          userId: 3,
+          status: 1,
+          totalPrice: 90000,
+          totalArea: 20,
+          totalDuration: 30,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          code: "#P-0003",
+          appointmentId: 9,
+          userId: 5,
+          status: -1,
+          totalPrice: 90000,
+          totalArea: 20,
+          totalDuration: 30,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
