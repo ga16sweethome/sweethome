4ba2b06e40ed1340507bca6d3a3a743515b68c52
affandiagung <ccoc.20001@gmail.com>
2022-02-20 15:41:02 +0700

    Test

--- controllers/showcase.js
+++ controllers/showcase.js
@@ -530,240 +530,7 @@ module.exports = {
       errorHandler(res, error);
     }
   },
-  createCompletedProject: async (req, res) => {
-    const body = req.body;
-    const user = req.user;
-    const files = req.files;
 
-    try {
-      //validasi apakah filesnya ada atau tidak
-      if (files.length === 0) {
-        return res.status(400).json({
-          status: "Bad Request",
-          message: `\"picture\" required files`,
-          result: {},
-        });
-      }
-
-      //create schema for Joi validate
-      const schema = Joi.object({
-        projectId: Joi.number().required(),
-        projectTotalPrice: Joi.number().required(),
-        projectTotalDuration: Joi.number().required(),
-        projectAppointmentAddress: Joi.string().required(),
-        projectAppointmentId: Joi.number().required(),
-        name: Joi.string().required(),
-        projectType: Joi.array().required().items(Joi.string().required()),
-        section: Joi.array().required().items(Joi.string().required()),
-        styles: Joi.array().required().items(Joi.string().required()),
-        title: Joi.array().required().items(Joi.string().required()),
-        picture: Joi.array(),
-      });
-
-      // validasi body dan files
-      const { error } = schema.validate({
-        ...body,
-        picture: files.path,
-      });
-      if (error) {
-        return res.status(400).json({
-          status: "Bad Request",
-          message: error.message,
-        });
-      }
-
-      // validasi title array  =  files array
-      if (body.title.length != files.length) {
-        return res.status(400).json({
-          status: "Bad Request",
-          message: "please fill all the data that required",
-          result: {},
-        });
-      }
-      const createShowcase = await Showcase.create({
-        name: body.name,
-        showcaseTypeId: 1,
-        projectId: body.projectId,
-        createdBy: user.id,
-        is_shown: false,
-      });
-
-      if (!createShowcase) {
-        return res.status(400).json({
-          status: "Bad Request",
-          message: "Create Showcase failed",
-          result: {},
-        });
-      }
-      const queryP = body.projectType;
-      const PTypeJunk = []; //untuk bulcreate ( array ) ke database ShowcaseJunkProjectType
-      if (queryP) {
-        const pType = await ProjectType.findAll({ where: { name: queryP } });
-        //validasi jika tidak ada data ProjectType
-        if (!pType) {
-          return res.status(400).json({
-            status: "Bad Request",
-            message: "Project Type is not found",
-            result: {},
-          });
-        }
-        const pTypeId = pType.map((el) => el.id);
-        for (let i = 0; i < pTypeId.length; i++) {
-          //validasi isi ProjectType.id
-          if (
-            pTypeId[i] === null ||
-            pTypeId[i] === undefined ||
-            queryP.length != pTypeId.length
-          ) {
-            return res.status(400).json({
-              status: "Bad request",
-              message: "ProjectType  is not match",
-              resykt: {},
-            });
-          }
-        }
-
-        for (let i = 0; i < pTypeId.length; i++) {
-          PTypeJunk.push({
-            showcaseId: createShowcase.id,
-            projectTypeId: pTypeId[i],
-          });
-        }
-      }
-      const buatJunkP = await ShowcaseJunkProjectType.bulkCreate(PTypeJunk);
-      //validasi gagal BulkCreate SHowcaseJunkProjectType
-      if (!buatJunkP) {
-        return res.status(400).json({
-          status: "Bad Request",
-          message: "Create ShowcaseJunkProjectType failed",
-          result: {},
-        });
-      }
-
-      const querySection = body.section;
-      const ssJunk = []; //untuk bulcreate ( array ) ke database ShowcaseJunkProjectType
-      if (querySection) {
-        const pSection = await Section.findAll({
-          where: { name: querySection },
-        });
-        //validasi jika tidak ada data Section
-        if (!pSection) {
-          return res.status(400).json({
-            status: "Bad Request",
-            message: "Section is not found",
-            result: {},
-          });
-        }
-        const ssId = pSection.map((el) => el.id);
-        for (let i = 0; i < ssId.length; i++) {
-          //validasi jika is section.id not null, not undefined  )
-          if (
-            ssId[i] === undefined ||
-            ssId[i] === null ||
-            querySection.length != ssId.length
-          ) {
-            return res.status(400).json({
-              Status: "Bad request",
-              Message: "Setion is not match",
-              result: {},
-            });
-          }
-        }
-
-        for (let i = 0; i < pSection.length; i++) {
-          ssJunk.push({
-            showcaseId: createShowcase.id,
-            sectionId: ssId[i],
-          });
-        }
-      }
-      const buatSSJunk = await ShowcaseJunkSection.bulkCreate(ssJunk);
-      if (!buatSSJunk) {
-        return res.status(400).json({
-          status: "Bad Request",
-          message: "Create ShowcaseJunkSection failed",
-          result: {},
-        });
-      }
-
-      const queryS = body.styles;
-      const StyleJunk = []; //untuk bulcreate ( array ) ke database ShowcaseJunkStyle
-      if (queryS) {
-        const style = await Style.findAll({ where: { name: queryS } });
-        if (!style) {
-          return res.status(400).json({
-            status: "Bad Request",
-            message: "Styles is not found",
-            result: {},
-          });
-        }
-        const styleId = style.map((el) => el.id);
-        for (let i = 0; i < styleId.length; i++) {
-          //validasi jika is section.id not null, not undefined  )
-          if (
-            styleId[i] === undefined ||
-            styleId[i] === null ||
-            queryS.length != styleId.length
-          ) {
-            return res.status(400).json({
-              status: "Bad request",
-              message: "Styles is not match",
-              result: {},
-            });
-          }
-        }
-        for (let i = 0; i < styleId.length; i++) {
-          StyleJunk.push({
-            showcaseId: createShowcase.id,
-            styleId: styleId[i],
-          });
-        }
-      }
-      const buatJunkS = await ShowcaseJunkStyle.bulkCreate(StyleJunk);
-      if (!buatJunkS) {
-        return res.status(400).json({
-          status: "Bad Request",
-          message: "Create ShowcaseJunStyle failed",
-          result: {},
-        });
-      }
-
-      const PGallery = []; //untuk bulcreate ( array ) ke database Gallery
-      for (let i = 0; i < files.length; i++) {
-        PGallery.push({
-          showcaseId: createShowcase.id,
-          title: body.title[i],
-          picture: files[i].path,
-        });
-      }
-      const buatGallery = await Gallery.bulkCreate(PGallery);
-      if (!buatGallery) {
-        return res.status(400).json({
-          status: "Bad Request",
-          message: "Create Gallery failed",
-          result: {},
-        });
-      }
-
-      const result = await Showcase.findOne({
-        where: { id: createShowcase.id },
-      });
-      if (!result) {
-        return res.status(400).json({
-          status: "Bad Request",
-          message: "Data Not Found",
-          result: {},
-        });
-      }
-      res.status(200).json({
-        status: "Succes",
-        message: "Successfully created Showcase Completed Project",
-        result: result,
-      });
-    } catch (error) {
-      errorHandler(res, error);
-    }
-  },
   createPortfolio: async (req, res) => {
     const body = req.body;
     const user = req.user;
@@ -959,6 +726,92 @@ module.exports = {
       res.status(200).json({
         status: "Succes",
         message: "Successfully created Showcase Portfolio",
+        result: result,
+      });
+    } catch (error) {
+      errorHandler(res, error);
+    }
+  },
+
+  createCompletedProject: async (req, res) => {
+    const body = req.body;
+    const user = req.user;
+    const files = req.files;
+
+    try {
+      //validasi apakah filesnya ada atau tidak
+      if (files.length === 0) {
+        return res.status(400).json({
+          status: "Bad Request",
+          message: `\"picture\" required files`,
+          result: {},
+        });
+      }
+
+      //create schema for Joi validate
+      const schema = Joi.object({
+        projectId: Joi.number().required(),
+        projectTotalPrice: Joi.number().required(),
+        projectTotalDuration: Joi.number().required(),
+        projectAppointmentAddress: Joi.string().required(),
+        projectAppointmentId: Joi.number().required(),
+        name: Joi.string().required(),
+        projectType: Joi.array().required().items(Joi.string().required()),
+        section: Joi.array().required().items(Joi.string().required()),
+        styles: Joi.array().required().items(Joi.string().required()),
+        title: Joi.array().required().items(Joi.string().required()),
+        picture: Joi.array(),
+      });
+
+      // validasi body dan files
+      const { error } = schema.validate({
+        ...body,
+        picture: files.path,
+      });
+      if (error) {
+        return res.status(400).json({
+          status: "Bad Request",
+          message: error.message,
+        });
+      }
+
+      // validasi title array  =  files array
+      if (body.title.length != files.length) {
+        return res.status(400).json({
+          status: "Bad Request",
+          message: "please fill all the data that required",
+          result: {},
+        });
+      }
+      const createShowcase = await Showcase.create({
+        name: body.name,
+        showcaseTypeId: 1,
+        projectId: body.projectId,
+        createdBy: user.id,
+        is_shown: false,
+      });
+
+      if (!createShowcase) {
+        return res.status(400).json({
+          status: "Bad Request",
+          message: "Create Showcase failed",
+          result: {},
+        });
+      }
+
+      const result = await Showcase.findOne({
+        where: { id: createShowcase.id },
+      });
+      if (!result) {
+        return res.status(400).json({
+          status: "Bad Request",
+          message: "Data Not Found",
+          result: {},
+        });
+      }
+      res.status(200).json({
+        status: "Succes",
+        message: "Successfully created Showcase Completed Project",
         result: result,
       });
     } catch (error) {
