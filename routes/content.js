b6ca25e13414f3579c25347d9e78da18a552473e
affandiagung <ccoc.20001@gmail.com>
2022-02-27 23:18:07 +0700

    add is admin in content

--- routes/content.js
+++ routes/content.js
@@ -10,17 +10,17 @@ const {
   getprojectType,
   createProjectType,
 } = require("../controllers/content");
-
+const { isAdmin } = require("../middlewares/userAuth");
 const router = require("express").Router();
 
 router.get("/servicetype", getServiceType);
-router.post("/servicetype", createServiceType);
+router.post("/servicetype", isAdmin, createServiceType);
 router.get("/buildingtype", getBuildingType);
-router.post("/buildingtype", createBuildingType);
+router.post("/buildingtype", isAdmin, createBuildingType);
 router.get("/section", getSection);
-router.post("/section", createSection);
+router.post("/section", isAdmin, createSection);
 router.get("/style", getStyle);
-router.post("/style", createStyle);
+router.post("/style", isAdmin, createStyle);
 router.get("/projectType", getprojectType);
-router.post("/projectType", createProjectType);
+router.post("/projectType", isAdmin, createProjectType);
 module.exports = router;
